e98a57ca9888ac073a1eeb475a5302805805c588
Nuri TIRAŞ <nuritiras@gmail.com>
2023-10-18 13:01:31 +0300

    asd

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte kucukSayi = 127;
+        short kisaSayi = 32767;
+        int tamSayi = 2147483647;
+        long uzunSayi = 9223372036854775807L;
+        System.out.println("byte: "+ kucukSayi);
+        System.out.println("short: " + kisaSayi);
+        System.out.println("int: " + tamSayi);
+        System.out.println("long: " + uzunSayi);
+    }
+}
